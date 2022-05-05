8a0343e06b5605c152abd7bdc5edbeb957bb5dff
Aniruddha Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2022-05-05 14:27:29 +0530

    defined basic problem model

--- backend/problems/models.py
+++ backend/problems/models.py
@@ -1,3 +1,20 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Problem(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=1000)
+    
+    created_at =  models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    draft = models.BooleanField(default=True)
+    
+    likes = models.PositiveIntegerField(default=0)
+    dislikes = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
