4ed1a3815a127b54899ac39b792d36203489291d
Aniruddha Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2022-05-05 18:59:41 +0530

    dummy PostLIst component

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,45 +1,12 @@
 import type { NextPage } from 'next'
-import { useState, useEffect } from 'react'
 
-import Problem from 'components/Problem'
-
-const API_URL = 'http://127.0.0.1:8000/api/v1'
-
-const fetchAPIData = async () => {
-  try {
-    const data = await fetch(API_URL)
-    const body = await data.json()
-    return body
-  } catch (err) {
-    console.error(err)
-    return []
-  }
-}
-
-interface IProblem {
-  title: string,
-  description: string
-}
+import ProblemList from 'components/ProblemList'
 
 const Home: NextPage = () => {
-  const [problems, setProblems] = useState<IProblem[]>([])
-
-  useEffect(() => {
-    fetchAPIData()
-      .then(data => setProblems(data))
-      .catch(err => console.error(err))
-  }, [])
-  
-  return (
-    <form onSubmit={() => alert('hi')}>
-      <p>Hello World!</p>
-      <textarea name="code" id="dummy" cols={30} rows={10} />
-      <button type="submit">Submit</button>
-      problems are: 
-      {
-        problems.map((problem, index) => (<Problem key={index} title={problem.title} description={problem.description} />))
-      }
-    </form>
+  return(
+    <>
+       <ProblemList />
+    </>
   )
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,16 @@
+const colors = {
+  'off-black': '#3f3a5a',
+}
+
 module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
     fontFamily: {
       sans: ['Poppins', 'ui-sans-serif', 'Helvetica Neue', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      colors,
+    },
   },
   plugins: [],
 }
