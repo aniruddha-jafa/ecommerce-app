46aaf940fbabd1f1c86edb1a97ef693e8e54f670
Aniruddha Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2022-05-05 17:09:03 +0530

    added tailwind css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./src/**/*.{ts,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['Poppins', 'ui-sans-serif', 'Helvetica Neue', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
