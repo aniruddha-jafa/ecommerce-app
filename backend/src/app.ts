a07e816938bbc6a834e631c70478fab83ea1c949
Aniruddha Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2022-06-16 16:07:13 +0530

    allow cors requests

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 
 import { userRouter } from './routes/index'
 
@@ -8,6 +9,7 @@ const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cors())
 
 // route handlers
 app.use('/api/users', userRouter)
