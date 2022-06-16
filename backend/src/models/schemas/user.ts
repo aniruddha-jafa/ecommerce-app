2153e2bf126796139c58e82a6f94f8b474ba23ae
Aniruddha Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2022-06-16 16:48:04 +0530

    add isAdmin field to user

--- backend/src/models/schemas/user.ts
+++ backend/src/models/schemas/user.ts
@@ -7,6 +7,7 @@ export const UserModel = z.object({
   name: z.string().max(200, { message: "Name must not exceed 200 characters" }).nullish(),
   hashedPassword: z.string().max(100, { message: "hashedPassword must not exceed 100 characters" }),
   status: z.nativeEnum(AccountStatus),
+  isAdmin: z.boolean(),
   isSeller: z.boolean(),
   createdAt: z.date(),
   updatedAt: z.date(),
