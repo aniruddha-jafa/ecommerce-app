2399ae6fd8e981599ca5acde33ee0326b7ee99cc
Aniruddha Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2022-06-16 16:07:03 +0530

    simplify frontend validation  because of backend

--- frontend/src/components/LoginForm/index.tsx
+++ frontend/src/components/LoginForm/index.tsx
@@ -13,9 +13,29 @@ import { LoginFormSchema, initialValues } from './schema'
 
 // ----------------------------------------------------------------
 
+// const API_URL = process.env.NEXT_PUBLIC_SERVER_DEV_URL
+
 /**@todo */
-const onSubmit = (values: object) => {
-  alert(JSON.stringify(values))
+const onSubmit = async (values: object) => {
+  console.log('mock submit with:', values)
+  // try {
+  //   if (!API_URL) {
+  //     throw new Error('API_URL is undefined')
+  //   }
+  //   const userEndpoint = API_URL + '/api/users'
+
+  //   let res = await fetch(userEndpoint, {
+  //     method: 'POST',
+  //     body: JSON.stringify(values),
+  //     headers: {
+  //       'Content-type': 'application/json',
+  //     },
+  //   })
+  //   // res = await res.json()
+  //   console.log('Received resposne: ', res)
+  // } catch (err: any) {
+  //   console.error('Something went wrong: ', err?.message)
+  // }
 }
 
 function validate(values: unknown) {
--- frontend/src/components/LoginForm/schema.ts
+++ frontend/src/components/LoginForm/schema.ts
@@ -2,21 +2,15 @@ import { z } from 'zod'
 
 // ----------------------------------------------------------------
 
-const EMAIL_MIN = 2
 const EMAIL_MAX = 250
-const PASSWORD_MIN = 8
 const PASSWORD_MAX = 200
 
 const LoginFormSchema = z.object({
   email: z
     .string()
-    .min(EMAIL_MIN, `Email is too short`)
     .max(EMAIL_MAX, `Email must not exceed ${EMAIL_MAX} characters`)
     .email('Please enter a valid email'),
-  password: z
-    .string()
-    .min(PASSWORD_MIN, 'Password is too short')
-    .max(PASSWORD_MAX, 'Password is too long'),
+  password: z.string().max(PASSWORD_MAX, 'Password is too long'),
 })
 
 const initialValues: LoginForm = {
