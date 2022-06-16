499ae3797d3bf960839b5385907f42c4eea97ded
Aniruddha Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2022-06-16 16:11:38 +0530

    frontend - mock homepage layout

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,27 @@
+import Footer from 'components/Footer/index'
+import Navbar from 'components/Navbar/index'
+
 import type { NextPage } from 'next'
 
+/**
+ * For layout see:
+ * {@link | https://web.dev/one-line-layouts/#04.-pancake-stack:-grid-template-rows:-auto-1fr-auto}
+ */
 const Home: NextPage = () => {
   return (
     <>
-      <button className='btn'>Button</button>
+      <div
+        className='
+      min-h-screen 
+      grid grid-rows-[auto_1fr_auto]
+      '
+      >
+        <Navbar />
+        <div>
+          <p>body</p>
+        </div>
+        <Footer />
+      </div>
     </>
   )
 }
