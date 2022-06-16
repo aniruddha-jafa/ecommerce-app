b0a7ec6071209acd80ea222cb670ec068daa62ef
Aniruddha Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2022-06-16 12:31:35 +0530

    update user model

--- backend/src/models/schemas/user.ts
+++ backend/src/models/schemas/user.ts
@@ -4,9 +4,8 @@ import { AccountStatus } from "@prisma/client"
 export const UserModel = z.object({
   id: z.string().uuid(),
   email: z.string().max(200, { message: "Email must not exceed 200 characters" }),
-  name: z.string().max(200, { message: "Name must not exceed 200 characters" }),
+  name: z.string().max(200, { message: "Name must not exceed 200 characters" }).nullish(),
   hashedPassword: z.string().max(100, { message: "hashedPassword must not exceed 100 characters" }),
-  salt: z.string(),
   status: z.nativeEnum(AccountStatus),
   isSeller: z.boolean(),
   createdAt: z.date(),
