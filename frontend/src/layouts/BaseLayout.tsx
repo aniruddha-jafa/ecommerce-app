5c3b525189306d38233090a173cef45fd9743eef
Aniruddha Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2022-07-13 17:18:04 +0530

    move base layout to chakra UI

--- frontend/src/layouts/BaseLayout.tsx
+++ frontend/src/layouts/BaseLayout.tsx
@@ -1,29 +1,29 @@
+import { Grid } from '@chakra-ui/react'
+
 import Footer from 'components/Footer/index'
 import Navbar from 'components/Navbar/index'
 
 import type { ReactNode } from 'react'
 
+// ----------------------------------------------------------------
+
 interface props {
   children: ReactNode
 }
 
-/**
- * For layout see:
+/** 
+ * Header + footer, & distribute remaining space to page contents
+ * 
  * {@link | https://web.dev/one-line-layouts/#04.-pancake-stack:-grid-template-rows:-auto-1fr-auto}
- */
+*/
 function Main({ children }: props) {
   return (
     <>
-      <div
-        className='
-      min-h-screen 
-      grid grid-rows-[auto_1fr_auto]
-      '
-      >
+      <Grid minH='100vh' gridAutoRows={'auto 1fr auto'}>
         <Navbar />
         <div>{children}</div>
         <Footer />
-      </div>
+      </Grid>
     </>
   )
 }
